3f522f57f9c4f74bd33b817a050672a83a442bc3
subinLee <salllena33@gmail.com>
2023-07-31 17:03:05 +0900

    width height 값 조정

--- src/app.js
+++ src/app.js
@@ -1,148 +1,165 @@
-import { Gauge } from "./gauge.js";
+import { Gauge } from './gauge.js';
 import { DEFAULT, ON, OFF, data, timestamp, colors } from './util.js';
-import { toggleEvent, downEvent, moveEvent, upEvent, changeDegreeEvent, blurEvent } from './event.js';
+import {
+  toggleEvent,
+  downEvent,
+  moveEvent,
+  upEvent,
+  changeDegreeEvent,
+  blurEvent,
+} from './event.js';
 
 export default class App {
-    constructor () {
-        this.canvas = document.createElement('canvas');
-        this.ctx = this.canvas.getContext('2d');
-        document.body.appendChild(this.canvas);
-        
-        // const offCanvas  =   document.createElement('canvas');
-        // const offscreen  =   offCanvas.transferControlToOffscreen();
-        // const worker =   new Worker("./worker.js");
-        // // => worker.js - not Found 
-        // worker.postMessage({ canvas: offscreen }, [ offscreen ]);
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    document.body.appendChild(this.canvas);
 
-        const div = document.createElement('div');
-        div.className = "inputDiv";
-        document.body.appendChild(div);
+    // const offCanvas  =   document.createElement('canvas');
+    // const offscreen  =   offCanvas.transferControlToOffscreen();
+    // const worker =   new Worker("./worker.js");
+    // // => worker.js - not Found
+    // worker.postMessage({ canvas: offscreen }, [ offscreen ]);
 
-        //각도 버튼
-        const degreeBtn180 = document.createElement('button');
-        degreeBtn180.className = "degreeBtn";
-        degreeBtn180.textContent = 180;
-        document.querySelector('.inputDiv').appendChild(degreeBtn180);
+    const div = document.createElement('div');
+    div.className = 'inputDiv';
+    document.body.appendChild(div);
 
-        const degreeBtn270 = document.createElement('button');
-        degreeBtn270.className = "degreeBtn";
-        degreeBtn270.textContent = 270;
-        document.querySelector('.inputDiv').appendChild(degreeBtn270);
+    //각도 버튼
+    const degreeBtn180 = document.createElement('button');
+    degreeBtn180.className = 'degreeBtn';
+    degreeBtn180.textContent = 180;
+    document.querySelector('.inputDiv').appendChild(degreeBtn180);
 
-        const degreeBtn360 = document.createElement('button');
-        degreeBtn360.className = "degreeBtn";
-        degreeBtn360.textContent = 360;
-        document.querySelector('.inputDiv').appendChild(degreeBtn360);
+    const degreeBtn270 = document.createElement('button');
+    degreeBtn270.className = 'degreeBtn';
+    degreeBtn270.textContent = 270;
+    document.querySelector('.inputDiv').appendChild(degreeBtn270);
 
-        //애니메이션 시작 버튼
-        const btn = document.createElement('button');
-        btn.className = "btn";
-        btn.textContent = "START";
-        document.body.appendChild(btn);
+    const degreeBtn360 = document.createElement('button');
+    degreeBtn360.className = 'degreeBtn';
+    degreeBtn360.textContent = 360;
+    document.querySelector('.inputDiv').appendChild(degreeBtn360);
 
-        this.dataInput = document.createElement('input');
-        this.dataInput.className = "dataInput";
-        this.dataInput.value = "0.0";
-        this.dataInput.readOnly = true;
-        document.body.appendChild(this.dataInput);
+    //애니메이션 시작 버튼
+    const btn = document.createElement('button');
+    btn.className = 'btn';
+    btn.textContent = 'START';
+    document.body.appendChild(btn);
 
-        this.init();
-        this.resize();
-        this.addEvent();
+    this.dataInput = document.createElement('input');
+    this.dataInput.className = 'dataInput';
+    this.dataInput.value = '0.0';
+    this.dataInput.readOnly = true;
+    document.body.appendChild(this.dataInput);
+
+    this.init();
+    this.resize();
+    this.addEvent();
+  }
+
+  init() {
+    this.active = DEFAULT;
+
+    this.preData = 0.0;
+    this.curData = data();
+
+    this.lastTime = timestamp();
+    this.gauge = new Gauge(); //초기 값
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth / 2;
+    this.stageHeight = document.body.clientHeight / 2;
+
+    const dpr = window.devicePixelRatio;
+
+    this.canvas.width = this.stageWidth * dpr;
+    this.canvas.height = (this.stageHeight / 1.5) * dpr;
+
+    //게이지에도 리사이즈가 적용 되도록 설정
+    this.gauge.resize(this.stageWidth, this.stageHeight);
+
+    //resize 될때마다 render()의 requestAnimationFrame 중지
+    if (this.rafId) {
+      cancelAnimationFrame(this.rafId);
     }
 
-    init () {
-        this.active = DEFAULT;
-        
-        this.preData = 0.0;
-        this.curData = data();
+    this.ctx.scale(dpr, dpr);
 
-        this.lastTime = timestamp();
-        this.gauge = new Gauge();    //초기 값
-    }
+    this.render();
+  }
 
-    resize () {
-        this.stageWidth     =   document.body.clientWidth;
-        this.stageHeight    =   document.body.clientHeight;
-        
-        const dpr = window.devicePixelRatio;
+  render() {
+    this.x = this.canvas.width;
+    this.y = this.canvas.height;
+    this.ctx.clearRect(0, 0, this.x, this.y);
 
-        this.canvas.width   =   this.stageWidth * dpr ;
-        this.canvas.height  =   this.stageHeight/1.5 * dpr ;
+    switch (this.active) {
+      case DEFAULT:
+        break;
 
-        //게이지에도 리사이즈가 적용 되도록 설정
-        this.gauge.resize(this.stageWidth, this.stageHeight);
-
-        //resize 될때마다 render()의 requestAnimationFrame 중지
-        if ( this.rafId ) {
-            cancelAnimationFrame(this.rafId);
+      case ON:
+        this.curTime = timestamp();
+        if (this.curTime - this.lastTime >= 5000) {
+          this.curData = data();
+          this.lastTime = timestamp();
+        }
+        this.dataInput.value = this.curData;
+        //수정
+        if (this.preData < this.curData) {
+          this.preData += this.gauge.speed;
+          if (this.preData >= this.curData) {
+            this.preData = this.curData;
+          }
+        } else if (this.preData > this.curData) {
+          this.preData -= this.gauge.speed;
+          if (this.preData <= this.curData) {
+            this.preData = this.curData;
+          }
         }
 
-        this.ctx.scale(dpr, dpr);
+        break;
 
-        this.render();
+      case OFF:
+        this.curData = this.preData;
+        break;
     }
 
-    render () {
-        this.x = this.canvas.width;
-        this.y = this.canvas.height;
-        this.ctx.clearRect(0, 0, this.x, this.y);
-        
-        
-        switch (this.active) {
-            case DEFAULT :
-                break;
-            
-            case ON :
-                this.curTime = timestamp();
-                if (this.curTime - this.lastTime >= 5000) {
-                    this.curData = data();
-                    this.lastTime = timestamp()
-                }
-                this.dataInput.value = this.curData;
-                //수정 
-                if ( this.preData < this.curData ) {
-                    this.preData += this.gauge.speed;
-                    if ( this.preData >= this.curData ) {
-                        this.preData = this.curData;
-                    }
-                } else if ( this.preData > this.curData ) {
-                    this.preData -= this.gauge.speed;
-                    if ( this.preData <= this.curData ) {
-                        this.preData = this.curData;
-                    }
-                }
+    this.gauge.color = colors[parseInt(this.preData / 10)];
+    this.gauge.percent = this.preData.toFixed(1);
+    this.gauge.draw(this.ctx);
 
-                break;
-            
-            case OFF :
-                this.curData = this.preData;
-                break;
-        }
+    this.rafId = requestAnimationFrame(this.render.bind(this));
 
-        this.gauge.color = colors[parseInt(this.preData/10)];
-        this.gauge.percent = this.preData.toFixed(1);
-        this.gauge.draw(this.ctx);
-
-        this.rafId = requestAnimationFrame(this.render.bind(this));
-
-        if (this.active === OFF || this.active === DEFAULT ){
-            cancelAnimationFrame(this.rafId);
-        }
+    if (this.active === OFF || this.active === DEFAULT) {
+      cancelAnimationFrame(this.rafId);
     }
-   
+  }
 
-    addEvent () {
-        window.addEventListener('resize', this.resize.bind(this));
-        window.addEventListener('blur', blurEvent.bind(this));
+  addEvent() {
+    window.addEventListener('resize', this.resize.bind(this));
+    window.addEventListener('blur', blurEvent.bind(this));
 
-        document.querySelector('.btn').addEventListener('click', toggleEvent.bind(this));
+    document
+      .querySelector('.btn')
+      .addEventListener('click', toggleEvent.bind(this));
 
-        document.querySelector('canvas').addEventListener('mousedown', downEvent.bind(this));
-        document.querySelector('canvas').addEventListener('mousemove', moveEvent.bind(this));
-        document.querySelector('canvas').addEventListener('mouseup', upEvent.bind(this));
-        document.querySelector('canvas').addEventListener('mouseleave', upEvent.bind(this));
+    document
+      .querySelector('canvas')
+      .addEventListener('mousedown', downEvent.bind(this));
+    document
+      .querySelector('canvas')
+      .addEventListener('mousemove', moveEvent.bind(this));
+    document
+      .querySelector('canvas')
+      .addEventListener('mouseup', upEvent.bind(this));
+    document
+      .querySelector('canvas')
+      .addEventListener('mouseleave', upEvent.bind(this));
 
-        document.querySelector('.inputDiv').addEventListener('click', changeDegreeEvent.bind(this));
-    }
+    document
+      .querySelector('.inputDiv')
+      .addEventListener('click', changeDegreeEvent.bind(this));
+  }
 }
--- src/event.js
+++ src/event.js
@@ -4,85 +4,97 @@ import { isInsideArc, pointDegree, toPercent } from './calc.js';
 let down = false;
 
 const toggleEvent = function (e) {
-    if (this.active === OFF || this.active === DEFAULT) {
-        this.active = ON;
-        e.target.textContent = "STOP";
-        
-    } else {
-        e.target.textContent = "START";
-        this.active = OFF;        
-    }
-    this.render();    
-}
+  if (this.active === OFF || this.active === DEFAULT) {
+    this.active = ON;
+    e.target.textContent = 'STOP';
+  } else {
+    e.target.textContent = 'START';
+    this.active = OFF;
+  }
+  this.render();
+};
 
 const downEvent = function (e) {
-    down = true;
-    
-    mousePos.x  =   e.layerX;
-    mousePos.y  =   e.layerY;
-    
-    const x = mousePos.x - this.gauge.circleX;
-    const y = mousePos.y - this.gauge.circleY;
+  down = true;
 
-    if ( !isInsideArc(x, y, this.gauge.radius) ) return;
-    
-    this.preData = toPercent(this.gauge.circleDegree, pointDegree(x, y, this.gauge.circleDegree));
+  mousePos.x = e.layerX;
+  mousePos.y = e.layerY;
 
-    this.ctx.clearRect(0, 0, this.x, this.y);
-    this.gauge.color    =   colors[parseInt(this.preData/10)];
-    this.gauge.percent  =   this.preData.toFixed(1);
-    this.gauge.draw(this.ctx);
-    
-    this.curData = this.preData;
-    this.curTime = timestamp();
-}
+  const x = mousePos.x - this.gauge.circleX;
+  const y = mousePos.y - this.gauge.circleY;
+
+  if (!isInsideArc(x, y, this.gauge.radius)) return;
+
+  this.preData = toPercent(
+    this.gauge.circleDegree,
+    pointDegree(x, y, this.gauge.circleDegree)
+  );
+
+  this.ctx.clearRect(0, 0, this.x, this.y);
+  this.gauge.color = colors[parseInt(this.preData / 10)];
+  this.gauge.percent = this.preData.toFixed(1);
+  this.gauge.draw(this.ctx);
+
+  this.curData = this.preData;
+  this.curTime = timestamp();
+};
 
 const moveEvent = function (e) {
-    if (!down) return;
-    
-    // downEvent 와 중복 - 수정 => 피드백 요청
-    mousePos.x  =   e.layerX;
-    mousePos.y  =   e.layerY;
+  if (!down) return;
 
-    const x = mousePos.x - this.gauge.circleX;
-    const y = mousePos.y - this.gauge.circleY;
+  // downEvent 와 중복 - 수정 => 피드백 요청
+  mousePos.x = e.layerX;
+  mousePos.y = e.layerY;
 
-    if ( !isInsideArc(x, y, this.gauge.radius) ) return;
-    
-    this.preData = toPercent(this.gauge.circleDegree, pointDegree(x, y, this.gauge.circleDegree));
+  const x = mousePos.x - this.gauge.circleX;
+  const y = mousePos.y - this.gauge.circleY;
 
-    this.ctx.clearRect(0, 0, this.x, this.y);
-    this.gauge.color    =   colors[parseInt(this.preData/10)];
-    this.gauge.percent  =   this.preData.toFixed(1);
-    this.gauge.draw(this.ctx);
+  if (!isInsideArc(x, y, this.gauge.radius)) return;
 
-    this.curData = this.preData;
-    this.curTime = timestamp();
-}
+  this.preData = toPercent(
+    this.gauge.circleDegree,
+    pointDegree(x, y, this.gauge.circleDegree)
+  );
+
+  this.ctx.clearRect(0, 0, this.x, this.y);
+  this.gauge.color = colors[parseInt(this.preData / 10)];
+  this.gauge.percent = this.preData.toFixed(1);
+  this.gauge.draw(this.ctx);
+
+  this.curData = this.preData;
+  this.curTime = timestamp();
+};
 
 const upEvent = function (e) {
-    down = false;
-}
+  down = false;
+};
 
 const changeDegreeEvent = function (e) {
-    if ( e.target.className !== "degreeBtn" ) return;
+  if (e.target.className !== 'degreeBtn') return;
 
-    for(const btn of document.querySelectorAll('.inputDiv button')){
-        btn.classList.remove('onDegree');
-    }
-    
-    e.target.classList.add('onDegree');
+  for (const btn of document.querySelectorAll('.inputDiv button')) {
+    btn.classList.remove('onDegree');
+  }
 
-    const degree = e.target.textContent;
-    this.gauge.circleDegree = degree;
-    this.render();
-}
+  e.target.classList.add('onDegree');
+
+  const degree = e.target.textContent;
+  this.gauge.circleDegree = degree;
+  this.render();
+};
 
 const blurEvent = function (e) {
-    if (this.active !== ON ) return;
-    document.querySelector('.btn').textContent = "START";
-    this.active = OFF; 
-    this.render();    
-}
+  if (this.active !== ON) return;
+  document.querySelector('.btn').textContent = 'START';
+  this.active = OFF;
+  this.render();
+};
 
-export { toggleEvent, downEvent, moveEvent, upEvent, changeDegreeEvent, blurEvent };
+export {
+  toggleEvent,
+  downEvent,
+  moveEvent,
+  upEvent,
+  changeDegreeEvent,
+  blurEvent,
+};
